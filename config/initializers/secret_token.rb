9a22f609633162d62c719d6b00b810da9a03d425
ernestopm <ernestopime@unitec.edu>
2015-06-19 09:56:06 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Facebook::Application.config.secret_key_base = '406fb9553c6f1ee409837056d1519ee9330ca00bf2d9744259f31cfed864eaee5252474d8225600b1a4bbe9155c85b72e5d2a22bed7d777a229754355d4f9842'
